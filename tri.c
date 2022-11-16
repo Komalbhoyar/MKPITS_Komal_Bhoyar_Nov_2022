05cb27deb673cccaa7bd0dcfca32f6f9bab81fbf
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2022-11-17 04:03:39 +0530

    calculate area of triangle

---
+++ tri.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main();
+{
+  int base,height,c;
+  clrscr();
+  printf("enter the value of base,height");
+  scanf("%d%d",&base,&height");
+  c=0.5*base*height;
+  printf("area of circle=%d",area);
+  getch();
+  
+}
