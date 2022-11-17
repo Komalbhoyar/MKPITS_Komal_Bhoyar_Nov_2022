f015132d0ddbd5495ba8755673d6856b7b3bd0db
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2022-11-17 07:11:25 +0530

    calculate area of rectangle

---
+++ RECT.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int lenght,width,c;
+clrscr();
+printf("enter the value length*width");
+scanf("%d%d",&lenght,&width);
+c=lenght*width;
+printf("area of rect =%d",c);
+getch();
+}
