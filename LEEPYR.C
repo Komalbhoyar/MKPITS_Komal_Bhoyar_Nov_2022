d8114567ad3ccfa9a54f9cc3cbb8adfe0ef2c883
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2022-11-20 10:02:44 +0530

    leep year or not

---
+++ LEEPYR.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main();
+{
+int yr;
+clrscr();
+printf("enter the leep year");
+scanf("%d",&yr);
+if(y%100==0)
+{
+printf("the given no leep year");
+}
+else
+{
+printf("the given no leep year");
+}
+getch();
+}
