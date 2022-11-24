66df46ee450c839f48a15c01c6cb438f05ed124b
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2022-11-24 23:11:41 +0530

    factorials

---
+++ FACT.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void fact()
+{
+int num,i,s=1;
+clrscr();
+printf("enter a num");
+scanf("%d",&num);
+for(i=1;i<=num;i++)
+{
+s=s*i;
+}
+printf("factorial=%d",s);
+}
+void main()
+{
+fact();
+getch();
+}
