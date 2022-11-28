a6a165e0ad803ad659ee1e231d4abcf34e1e599a
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2022-11-28 21:17:23 +0530

    function overloading

---
+++ FUNOVERL.C
@@ -0,0 +1,19 @@
+#include<iostream.h>
+#include<conio.h>
+int sum(int a,int b)
+{
+return(a+b)
+}
+int sum(int a,int b,int())
+{
+return(a+b+c);
+}
+int sum(int a,int b,int c,int d)
+{
+return((a+b+c+d);
+}
+void main()
+{
+cout<<sum(2,3,4,5);
+getch();
+}
