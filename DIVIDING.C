76d6ca47cea2f6fb6b39a66aecf3091a613668d6
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2022-11-29 23:47:18 +0530

    dividing it by 7 and the remainder is equal to 2 or 3 betwn 2 integer

---
+++ DIVIDING.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int x,y,temp,i;
+clrscr();
+printf("\n input the first integer");
+scanf("%d",&x);
+printf("\n input the second integer");
+scanf("%d",&y);
+if(x>y)
+{
+temp=y;
+y=x;
+x=temp;
+}
+for(i=x+1;i<y;i++)
+{
+if((i%7)==2||(i%7)==3)
+{
+printf("%d\n",i);
+}
+}
+getch();
+return 0;
+}
