e17cbc44515eeb457d7c5eed0e77e0b96fcc2716
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2022-12-12 11:27:34 +0530

    function

---
+++ 10.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+add()
+{
+int x,y,z;
+clrscr();
+printf("enter value of x & y");
+scanf("%d%d",&x,&y);
+z=x+y;
+printf("%d",z);
+}
+main()
+{
+add();
+}
+getch();
+}
