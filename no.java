2c8003e081ce516a2739092b5f63cc822a7e192f
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2023-01-15 22:48:32 +0530

    if else in java

---
+++ no.java
@@ -0,0 +1,16 @@
+class no
+{
+   public static void main(String[] args)
+   {
+   int num=7;
+   if(num>0)
+   {
+   System.out.println("enter your no");
+   System.out.println("the no is +ve");
+   }
+   else
+    {
+     System.out.println("the no is -ve");
+    }
+   }
+}
