da7cf6a83dd12afab28553b48d0fce6c4187dce0
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2023-01-15 22:43:09 +0530

    if in java

---
+++ divisible.java
@@ -0,0 +1,14 @@
+ class divisible
+{
+ public static void main(String[] args)
+  {
+  int num=8;
+  int r=num%4;
+  if(num%4==0)
+   {
+   System.out.println("enter your no");
+   System.out.println("divide="+r);
+   }
+  }
+}
+
