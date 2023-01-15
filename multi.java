55bd0507560b3e4bc65fb52e4b6b43b5469544a9
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2023-01-15 22:18:46 +0530

    multiplication in java

---
+++ multi.java
@@ -0,0 +1,32 @@
+ class multi
+ {
+ public static void main(String[] args)
+ {
+ int a=3,b=5,c;
+ c=a*b;
+ System.out.println("enter your no");
+ System.out.println("multi="+c);
+ }
+}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
