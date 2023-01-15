86c670323454eab59f804ced214b4f54528e12cc
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2023-01-15 22:23:32 +0530

    formula using java
    area of circle=3.14*r*r

---
+++ circle.java
@@ -0,0 +1,10 @@
+ class circle
+ {
+ public static void main(String[] args)
+ {
+ int r=5;
+ double area=3.14*(r*r);
+ System.out.println("enter the area of circle");
+ System.out.println("area of circle="+area);
+ }
+}
