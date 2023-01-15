3355a65348f45ae25949c31718152f1932cc838e
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2023-01-15 22:20:13 +0530

    swpping in java

---
+++ swpping.java
@@ -0,0 +1,12 @@
+ class swpping
+ {
+ public static void main(String [] args)
+ {
+ int num1=3,num2=4,swp;
+ swp=num2;
+ num2=num1;
+ num1=swp;
+ System.out.println("enter the num1,num2");
+ System.out.println("swapping="+swp);
+ }
+}
