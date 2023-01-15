9f5323dbdeb79daa580b9d7893dd5aaa9e47862c
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2023-01-15 22:44:26 +0530

    if else in java

---
+++ greatest.java
@@ -0,0 +1,16 @@
+class greatest
+{
+ public static void main(String[] args)
+ {
+ int a=5,b=7;
+ if(a>b)
+     {
+     System.out.println("enter your no a & b");
+     System.out.println("a is greater than b");
+     }
+     else
+     {
+     System.out.println("b is greater than a");
+     }
+ }
+}
