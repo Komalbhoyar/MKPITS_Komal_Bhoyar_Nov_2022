a7d698ae85559d1549814ea2fc4e982f5f36c2ba
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2023-01-15 22:42:03 +0530

    nested if else in java

---
+++ age.java
@@ -0,0 +1,22 @@
+class age
+{
+ public static void main(String[] args)
+ {
+  int p1=6,p2=8,p3=5;
+  if(p1>p2 && p1>p3)
+  {
+  System.out.println("enter the age of no");
+  System.out.println("p1 is greater");
+  }
+  else
+  if(p2>p3 && p2>p1)
+  {
+  System.out.println("p2 is greater");
+  }
+  else
+  if(p3>p1 && p3>p2)
+  {
+  System.out.println("p3 is greater");
+  }
+ }
+}
