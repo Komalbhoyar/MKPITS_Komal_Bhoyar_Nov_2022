1968d22531ea379f62b7364f5a290d063c2f07c2
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2023-01-15 22:30:16 +0530

    rectangle formula in java
    area of rectangle=lenght*width

---
+++ rectangle.java
@@ -0,0 +1,10 @@
+ class rectangle
+ {
+ public static void main(String[] args)
+ {
+ int lenght=5,width=8,c;
+ c=lenght*width;
+ System.out.println("enter your value lenght & width");
+ System.out.println("area of rectangle="+c);
+ }
+}
