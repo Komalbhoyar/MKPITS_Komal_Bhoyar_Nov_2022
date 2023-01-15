d5488ee8a32aae252470ef582bc3ad52c017de14
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2023-01-15 23:04:14 +0530

    starparttern in java

---
+++ Starpattern.java
@@ -0,0 +1,15 @@
+class Starpattern
+{
+ public static void main(String[] args)
+ {
+  int r,c;
+  for(r=1;r<=4;r++)
+  {
+  for(c=1;c<=r;c++)
+  {
+  System.out.print("*");
+  }
+  System.out.println("");;
+  }
+ }
+}
