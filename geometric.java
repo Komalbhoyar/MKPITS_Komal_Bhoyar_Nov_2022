cdae572f30457516bcbdca9046511244e3befcfa
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2023-01-17 18:14:02 +0530

    Geomatric series in java

---
+++ geometric.java
@@ -0,0 +1,19 @@
+class Geo
+{
+int i;
+void set()
+{
+for(i=5;i<=100;i=i+5)
+{
+System.out.println(i);
+}
+}
+}
+class geometric
+{
+public static void main(String[] args)
+{
+Geo ob1=new Geo();
+ob1.set();
+}
+}
