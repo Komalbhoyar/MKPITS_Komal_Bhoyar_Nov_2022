2a75e98455b6b27b0181c37b972507ed21c9ab1b
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2023-01-17 08:10:17 +0530

    using class and objects in java

---
+++ one2.java
@@ -0,0 +1,22 @@
+class adddemo
+{
+	int a=8,b=8;
+	int res;
+	void sum()
+	{
+		res=a+b;
+	}
+	void dis()
+	{
+		System.out.println(res);
+	}
+}
+public class one2
+{
+	public static void main(String[] args)
+	{
+		adddemo ob=new adddemo();
+		ob.sum();
+		ob.dis();
+	}
+}
