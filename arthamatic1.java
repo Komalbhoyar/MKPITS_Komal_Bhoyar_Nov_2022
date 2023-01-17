c52b7ae8c4e93356a065e47aa9868de8480ca025
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2023-01-17 18:11:27 +0530

    Arithamatic series in java

---
+++ arthamatic1.java
@@ -0,0 +1,19 @@
+class arth
+{
+int i;
+void set()
+{
+for(i=1;i<=30;i=i+3)
+{
+System.out.println(i);
+}
+}
+}
+class Arthmatic
+{
+public static void main(String[] args)
+{
+arth ob1=new arth();
+ob1.set();
+}
+}
