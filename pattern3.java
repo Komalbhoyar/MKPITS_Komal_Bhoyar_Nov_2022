26fa88574a208480b62046e2b5062d25d9fa21a9
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2023-01-18 22:30:35 +0530

    pattern program in java
             *                 *                 *                   *
          *     *          *     *           *     *             *      *
        *         *     *           *     *            *      *             *
      *               *                 *                   *                     *

---
+++ pattern3.java
@@ -0,0 +1,93 @@
+class pattern3
+{
+  public static void main(String[] args)
+  {
+	  //int i,j;
+
+   for( int i=0;i<=3;i++)
+   {
+     for(int j=0;j<=3;j++)
+     {
+      if(i+j==3)
+      {
+	   System.out.print("*");
+      }
+       else
+        System.out.print(" ");
+     }
+
+    for(int j=4;j<=6;j++)
+    {
+     if(j-i==3)
+    {
+	 System.out.print("*");
+    }
+    else
+     System.out.print(" ");
+    }
+
+    for(int j=7;j<=9;j++)
+    {
+	if(i+j==9)
+    {
+     System.out.print("*");
+    }
+	else
+     System.out.print(" ");
+    }
+
+  for(int j=10;j<=12;j++)
+   {
+	 if(j-i==9)
+	{
+	 System.out.print("*");
+    }
+	else
+	 System.out.print(" ");
+  }
+
+  for(int j=13;j<=15;j++)
+  {
+	  if(i+j==15)
+	  {
+		  System.out.print("*");
+	  }
+	  else
+	  System.out.print(" ");
+  }
+
+  for(int j=16;j<=18;j++)
+  {
+	  if(j-i==15)
+	  {
+		  System.out.print("*");
+	  }
+	  else
+	  System.out.print(" ");
+  }
+
+  for(int j=19;j<=21;j++)
+  {
+	  if(i+j==21)
+	  {
+		  System.out.print("*");
+	  }
+	  else
+	  System.out.print(" ");
+  }
+
+  for(int j=22;j<=24;j++)
+    {
+  	  if(j-i==21)
+  	  {
+  		  System.out.print("*");
+  	  }
+  	  else
+  	  System.out.print(" ");
+  }
+
+  System.out.println();
+  }
+ }
+}
+
