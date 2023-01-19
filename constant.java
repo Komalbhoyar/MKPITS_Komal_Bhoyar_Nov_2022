42b1427179f85037c3ae1991c48348eedd6eacc4
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2023-01-19 18:23:20 +0530

    constant array in java

---
+++ constant.java
@@ -0,0 +1,23 @@
+class arraydemo
+{
+void fun()
+{
+int a[]={1,2,3,4};
+int []a1;
+for(int i=0;i<=3;i++)
+{
+System.out.println(a[i]);
+}
+}
+}
+public class constant
+{
+
+public static void main(String[] args)
+{
+arraydemo ob1=new arraydemo();
+arraydemo ob2=new arraydemo();
+ob1.fun();
+ob2.fun();
+}
+}
