5d2643f0f2a2f3515b53a2b815f185d5cc313572
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2023-01-20 10:45:32 +0530

    final keyword-(using final to prevent overriding

---
+++ final1.java
@@ -0,0 +1,19 @@
+class one1
+{
+  one1(int x, int y)
+{
+System.out.println(x+ "" +y);
+}
+}
+
+
+public class final1
+{
+public static void main(String[] args)
+{
+final int a=5;
+System.out.println(a);
+a=6;
+System.out.println(a);
+}
+}
