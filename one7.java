62e29425002a17ba4129673b9f9f62d99f1254bb
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2023-01-20 10:27:19 +0530

    Inheritance - single inheritance in java

---
+++ one7.java
@@ -0,0 +1,24 @@
+class one1
+{
+int a;
+}
+class two extends one1
+{
+void set()
+{
+a=12;
+}
+void dis()
+{
+System.out.println(a);
+}
+}
+class one7
+{
+public static void main(String[] args)
+{
+two ob=new two();
+ob.set();
+ob.dis();
+}
+}
