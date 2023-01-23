51540399b2bb766f023c4b21db30831638160092
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2023-01-23 21:30:10 +0530

    WAP to print the result of the specified operation
    -1+4*6
    (35+5)%7
    14+(-4)*6/11
    2+15/16*1-7%2

---
+++ operation1.java
@@ -0,0 +1,20 @@
+class operation1
+{
+public static void main(String[] args)
+{
+
+int res=-1+4*6;
+System.out.println(res);
+
+int res1=(35+5)%7;
+System.out.println(res1);
+
+int res2=14+(-4)*6/11;
+System.out.println(res2);
+
+int res3=2+(15/6)*(1-7)%2;
+System.out.println(res3);
+
+
+}
+}
