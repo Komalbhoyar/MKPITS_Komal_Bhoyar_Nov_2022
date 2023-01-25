5da44c8b92c3e91a77aece1d437a605302a3ca7a
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2023-01-25 10:33:08 +0530

    reverse pro in java

---
+++ jan23_Q14.java
@@ -0,0 +1,17 @@
+class jan23_Q14
+{
+public static void main(String[] args)
+{
+int arr[]={1,2,3,4,5};
+int i,n=5;
+for(i=0;i<n;i++)
+{
+System.out.println(arr[i]);
+}
+System.out.println("reverse array");
+for(i=n;i>=0;i--)
+{
+System.out.println(arr[i]);
+}
+}
+}
