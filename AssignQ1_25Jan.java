d2c30f73fe7a8620bd30407b011914faf69486de
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2023-01-27 12:33:08 +0530

    WAP sum of two given array of integer len 3 & find largest sum

---
+++ AssignQ1_25Jan.java
@@ -0,0 +1,27 @@
+class AssignQ1_25Jan
+{
+public static void main(String[] args)
+{
+int a[]={5,6,8};
+int b[]={6,4,3};
+int sum1,sum2;
+sum1=(a[0]+a[1]+a[2]);
+System.out.println("enter the 1st array="+sum1);
+sum2=(b[0]+b[1]+b[2]);
+System.out.println("enter the 2nd array="+sum2);
+
+if(sum1>sum2)
+{
+	System.out.println("sum1 is  greater no="+sum1);
+}
+else
+{
+	System.out.println("sum2 is greater no="+sum2);
+}
+}
+}
+
+
+
+
+
