86d77fb67008c90452e8c8a790f3e3eda74fa40d
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2023-01-29 19:14:55 +0530

    WAP to create an array taking two middle element from given array if integer of length even

---
+++ assiQ2_25jan.java
@@ -0,0 +1,27 @@
+class assiQ2_25jan
+{
+public static void main(String[] args)
+{
+int a[]={1,2,3,4,5,6};
+int x,y;
+if(a.length%2==0)
+{
+System.out.println("length of the even no="+a.length);
+x=a[a.length/2-1];
+System.out.println("the middle term="+x);
+y=a[a.length/2];
+System.out.println("the middle term="+y);
+
+System.out.println("the middle term array is="+x+" "+y);
+}
+else
+{
+	System.out.println("no is not even");
+}
+}
+}
+
+
+
+
+
