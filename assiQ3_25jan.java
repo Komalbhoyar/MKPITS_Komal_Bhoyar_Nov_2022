8c95f4b0ce5b4d19cfcb21bbfeef5c3810f862a1
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2023-01-29 19:17:34 +0530

    WAP to create a new array form two give  array of integer each length 3

---
+++ assiQ3_25jan.java
@@ -0,0 +1,17 @@
+class assiQ3_25jan
+{
+public static void main(String[] args)
+{
+int a[]={2,4,6};
+int b[]={4,3,7};
+int c[]={a[0],a[1],a[2],b[0],b[1],b[2]};
+
+for(int i=0;i<c.length;i++)
+{
+System.out.println(" "+c[i]);
+}
+System.out.println();
+}
+}
+
+
