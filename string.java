eb0aedf6498d75f16424390f1d3e1c987482dfb8
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2023-02-16 05:05:45 +0530

    string in java

---
+++ string.java
@@ -0,0 +1,15 @@
+class string1
+{
+public static void main(String[] args)
+{
+char data[]={'a','b','c'};
+String sob=new String(data);
+System.out.println(sob);
+int len=sob.lenght();
+System.out.println(len);
+String ob1="komal";
+String ob2=new String();
+ob2=ob1;
+System.out.println(ob2);
+}
+}
