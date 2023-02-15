eacc847e4b78dc6a1482fa4c26ca77a626ae7aea
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2023-02-16 05:07:29 +0530

    range two no in java

---
+++ range.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class sum
+{
+public static void main(String[] args)
+{
+  Scanner scanner=new Scanner(System.in);
+  System.out.println("enter the two no");
+ int num1=scanner.nextInt();
+ System.out.println("enter the value num1");
+ int num2=scanner.nextInt();
+
+
+ int res=num1+num2;
+ {
+ System.out.println(res);
+}
+
+
+    if(res>=10 && res>=20)
+    {
+	System.out.println(" the enter number is  range "+30);
+
+	}
+}
+}
+
