606c283f0e54d1f365b33c96eb3fb0e4fe1effc9
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2023-02-16 05:17:32 +0530

    assiQ6_jan23 in java

---
+++ assiQ6_jan23.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class assiQ6_jan23
+{
+public static void main(String[] args)
+{
+	int a,b,c;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter your two no");
+ a=sc.nextInt();
+ b=sc.nextInt();
+ c=sc.nextInt();
+
+if(a+b==c)
+{
+System.out.println("it is a possible a+b=c");
+System.out.println((a+b)+"="+c);
+}
+}
+}
