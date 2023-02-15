2d7dc86a083c89a391b1a8b78dc7b6ff31f387dc
Komalbhoyar <118259168+Komalbhoyar@users.noreply.github.com>
2023-02-16 05:14:47 +0530

    assiQ18_jan23 in java

---
+++ assiQ18_jan23.java
@@ -0,0 +1,17 @@
+class assiQ18_jan23
+{
+public static void main(String[] args)
+{
+int a[]={10,15};
+boolean check=false;
+if(a[0]!=15 && a[0]!=20 && a[1]!=15 && a[1]!=20)
+{
+check=true;
+System.out.println(check);
+}
+else
+{
+System.out.println(check);
+}
+}
+}
