12fe15d52fc705eed2fccb9418170a37d5af2f20
komal bhoyar <komalbhoyar790@gmail.com>
2023-03-01 23:48:08 +0530

    display even no java

---
+++ Display_Even_no.java
@@ -0,0 +1,16 @@
+class Display_Even_no
+{
+public static void main(String[] args)
+{
+int a=100;
+System.out.println("list of even no 1 to 100");
+
+for( int i=1;i<a;i++)
+{
+if(i%2==0)
+{
+System.out.println("even no="+i);
+}
+}
+}
+}
