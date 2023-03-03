36f80f361d0e0bbd0a72fde8f3b22242d0620b27
komal bhoyar <komalbhoyar790@gmail.com>
2023-03-03 07:47:45 +0530

    array length java

---
+++ length_array.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class length_array
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+int a[]=new int[10];
+int arraylength=a.length;
+System.out.println("the length of the array="+a);
+}
+}
+
