3b729a6f8545a122981cb5a64597dbec7bca4385
komal bhoyar <komalbhoyar790@gmail.com>
2023-03-09 21:31:02 +0530

    sum of array

---
+++ sum_of_array.java
@@ -0,0 +1,14 @@
+class sum_of_array
+{
+public static void main(String[] args)
+{
+int arr[]={5,8,3,6,7,9,2};
+int sum=0;
+System.out.println("sum of array:");
+for(int i=0;i<arr.length;i++)
+{
+sum=sum+arr[i];
+}
+System.out.print(sum);
+}
+}
