1cb50fb4f531541e58260277d92d314224120e05
komal bhoyar <komalbhoyar790@gmail.com>
2023-03-09 18:25:32 +0530

    rev character in java

---
+++ rev.java
@@ -0,0 +1,11 @@
+class rev
+//WAP to print the following character in the reverse way
+{
+public static void main(String[] args)
+{
+StringBuffer sb=new StringBuffer("XML");
+System.out.println(sb);
+sb.reverse();
+System.out.print("the reverse XML="+sb);
+}
+}
