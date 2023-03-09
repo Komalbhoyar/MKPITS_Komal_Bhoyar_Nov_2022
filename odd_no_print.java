498e50742da5b1c49bb411ad0fe40d41476d4f8a
komal bhoyar <komalbhoyar790@gmail.com>
2023-03-09 21:05:54 +0530

    odd no display

---
+++ odd_no_print.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class odd_no_print
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the no of element");
+int size=sc.nextInt();
+int arr[]=new int[size];
+System.out.println("enter the element of the array");
+for(int i=0;i<size;i++)
+{
+	arr[i]=sc.nextInt();
+	System.out.println("odd number in array ");
+}
+	for(int i=0;i<size;i++)
+	{
+		if((arr[i]%2)!=0)
+		System.out.print(arr[i]+ " ");
+	}
+}
+}
+
