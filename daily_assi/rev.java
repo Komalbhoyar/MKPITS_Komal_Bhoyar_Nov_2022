f171b34994d0b637c73132dfe2ae8051feaca231
komal bhoyar <komalbhoyar790@gmail.com>
2023-03-10 22:48:53 +0530

    reverse array

---
+++ daily_assi/rev.java
@@ -0,0 +1,16 @@
+class rev
+{
+public static void main(String[] args)
+{
+int a[]={2,3,4,6,7,8,9};
+int n=6;
+for(int i=0;i<n;i++)
+{
+System.out.println(a[i]);
+}
+for(int i=n;i>=0;i--)
+{
+System.out.println(a[i]);
+}
+}
+}
