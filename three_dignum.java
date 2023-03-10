1424465540f183d35817eb8834292766d70a46b6
komal bhoyar <komalbhoyar790@gmail.com>
2023-03-10 23:18:14 +0530

    three digit no

---
+++ three_dignum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class three_dignum
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the three digit number");
+int a=sc.nextInt();
+int b=sc.nextInt();
+int c=sc.nextInt();
+
+int Avg;
+Avg=(a+b+c)/3;
+System.out.println("three digit number="+Avg);
+}
+}
