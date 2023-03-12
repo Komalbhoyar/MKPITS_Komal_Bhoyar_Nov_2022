c6a3fba146607012d9197c8e45833cf6aaa5fc45
komal bhoyar <komalbhoyar790@gmail.com>
2023-03-12 21:08:11 +0530

    using loop

---
+++ using_for_loop.java
@@ -0,0 +1,13 @@
+class using_for_loop
+{
+public static void main(String[] args)
+{
+int i=1;
+System.out.println("enter the number");
+
+for(i=1;i<=10;i++)
+{
+System.out.println("komal");
+}
+}
+}
